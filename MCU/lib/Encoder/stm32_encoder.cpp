f5adc414fb9121363aab4de85aa06f01d0a4eb3f
kubot080301 <kubot100601@gmail.com>
2021-09-01 16:51:21 +0800

    to publish

--- MCU/lib/Encoder/stm32_encoder.cpp
+++
@@ -1,9 +0,0 @@
-#include "stm32_encoder.h"
-
-// Yes, all the code is in the header file, to provide the user
-// configure options with #define (before they include it), and
-// to facilitate some crafty optimizations!
-
-Stm32_Encoder_internal_state_t * Stm32_Encoder::interruptArgs[];
-
-
--- MCU/lib/IMU/imu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class IMU
-{
-public:
-    virtual bool init() = 0;
-    virtual void get_data(float* imu_data) = 0;
-};
